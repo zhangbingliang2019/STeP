37e6224122801b0bf2cbb2166d745e6c5d1feb66
zbl <58138226+zhangbingliang2019@users.noreply.github.com>
2025-04-09 01:58:20 -0700

    update

--- js/results.js
+++ js/results.js
@@ -3,42 +3,42 @@ var bhvr_items = [
         steps: [
             { video: "000003/Ground truth.mp4", label: "Ground truth" },
             { video: "000003/STeP.mp4", label: "Ours" },
-            { video: "000003/IDM+BCS.mp4", label: "Baseline (based on image diffusion)" },
+            { video: "000003/IDM+BCS.mp4", label: "Baseline (image diffusion)" },
         ]
     },
     {
         steps: [
             { video: "000012/Ground truth.mp4", label: "Ground truth" },
             { video: "000012/STeP.mp4", label: "Ours" },
-            { video: "000012/IDM+BCS.mp4", label: "Baseline (based on image diffusion)" },
+            { video: "000012/IDM+BCS.mp4", label: "Baseline (image diffusion)" },
         ]
     },
     {
         steps: [
             { video: "000046/Ground truth.mp4", label: "Ground truth" },
             { video: "000046/STeP.mp4", label: "Ours" },
-            { video: "000046/IDM+BCS.mp4", label: "Baseline (based on image diffusion)" },
+            { video: "000046/IDM+BCS.mp4", label: "Baseline (image diffusion)" },
         ]
     },
     {
         steps: [
             { video: "000053/Ground truth.mp4", label: "Ground truth" },
             { video: "000053/STeP.mp4", label: "Ours" },
-            { video: "000053/IDM+BCS.mp4", label: "Baseline (based on image diffusion)" },
+            { video: "000053/IDM+BCS.mp4", label: "Baseline (image diffusion)" },
         ]
     },
     {
         steps: [
             { video: "000067/Ground truth.mp4", label: "Ground truth" },
             { video: "000067/STeP.mp4", label: "Ours" },
-            { video: "000067/IDM+BCS.mp4", label: "Baseline (based on image diffusion)" },
+            { video: "000067/IDM+BCS.mp4", label: "Baseline (image diffusion)" },
         ]
     },
     {
         steps: [
             { video: "000072/Ground truth.mp4", label: "Ground truth" },
             { video: "000072/STeP.mp4", label: "Ours" },
-            { video: "000072/IDM+BCS.mp4", label: "Baseline (based on image diffusion)" },
+            { video: "000072/IDM+BCS.mp4", label: "Baseline (image diffusion)" },
         ]
     },
 ];
@@ -48,42 +48,42 @@ var dmri_items = [
         steps: [
             { video: "000003/Ground truth.mp4", label: "Ground truth" },
             { video: "000003/STeP.mp4", label: "Ours" },
-            { video: "000003/IDM+BCS.mp4", label: "Baseline (based on image diffusion)" },
+            { video: "000003/IDM+BCS.mp4", label: "Baseline (image diffusion)" },
         ]
     },
     {
         steps: [
             { video: "000016/Ground truth.mp4", label: "Ground truth" },
             { video: "000016/STeP.mp4", label: "Ours" },
-            { video: "000016/IDM+BCS.mp4", label: "Baseline (based on image diffusion)" },
+            { video: "000016/IDM+BCS.mp4", label: "Baseline (image diffusion)" },
         ]
     },
     {
         steps: [
             { video: "000007/Ground truth.mp4", label: "Ground truth" },
             { video: "000007/STeP.mp4", label: "Ours" },
-            { video: "000007/IDM+BCS.mp4", label: "Baseline (based on image diffusion)" },
+            { video: "000007/IDM+BCS.mp4", label: "Baseline (image diffusion)" },
         ]
     },
     {
         steps: [
             { video: "000011/Ground truth.mp4", label: "Ground truth" },
             { video: "000011/STeP.mp4", label: "Ours" },
-            { video: "000011/IDM+BCS.mp4", label: "Baseline (based on image diffusion)" },
+            { video: "000011/IDM+BCS.mp4", label: "Baseline (image diffusion)" },
         ]
     },
     {
         steps: [
             { video: "000009/Ground truth.mp4", label: "Ground truth" },
             { video: "000009/STeP.mp4", label: "Ours" },
-            { video: "000009/IDM+BCS.mp4", label: "Baseline (based on image diffusion)" },
+            { video: "000009/IDM+BCS.mp4", label: "Baseline (image diffusion)" },
         ]
     },
     {
         steps: [
             { video: "000013/Ground truth.mp4", label: "Ground truth" },
             { video: "000013/STeP.mp4", label: "Ours" },
-            { video: "000013/IDM+BCS.mp4", label: "Baseline (based on image diffusion)" },
+            { video: "000013/IDM+BCS.mp4", label: "Baseline (image diffusion)" },
         ]
     },
 ];
@@ -102,7 +102,7 @@ function bhvr_carousel_item_template(item) {
                     </div>
                     <div class="x-column">
                         <div style="width: 100%; aspect-ratio: 1; overflow: hidden; border-radius: 8px;">
-                            <video autoplay playsinline loop muted height="100%" src="/assets/bhvr-videos/${step.video}"></video>
+                            <video autoplay playsinline loop muted height="100%" src="assets/bhvr-videos/${step.video}"></video>
                         </div>
                     </div>
                 </div>`;
@@ -125,7 +125,7 @@ function dmri_carousel_item_template(item) {
                     </div>
                     <div class="x-column">
                         <div style="width: 100%; aspect-ratio: 1; overflow: hidden; border-radius: 8px;">
-                            <video autoplay playsinline loop muted height="100%" src="/assets/dmri-videos/${step.video}"></video>
+                            <video autoplay playsinline loop muted height="100%" src="assets/dmri-videos/${step.video}"></video>
                         </div>
                     </div>
                 </div>`;
